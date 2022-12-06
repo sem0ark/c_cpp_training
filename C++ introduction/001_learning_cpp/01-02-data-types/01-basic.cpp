798958a9104c60a0e25cbe2102fc01f848c2119b
sem0ark <arcadii.sem@gmail.com>
2022-12-06 10:04:33 +0200

    Add information about C++ types

---
+++ C++ introduction/001_learning_cpp/01-02-data-types/01-basic.cpp
@@ -0,0 +1,34 @@
+// Code notes on the C++ inroduction course from LinkedIn learning
+// Created by Arkadii Semenov, 2022.12.06
+
+#include <iostream>
+
+// Data types
+
+// Integers:
+// - signed / unsigned
+// - char (8 bits)
+// - int (implementation dependent)
+// - NB! -> stdint.h contains types like uint32_t
+//          Use them for readable and independent
+//          integers of specified properties
+
+// Floating point numbers:
+//  used for real numbers
+//  IEEE 754 floating point standard
+// - float (light and imprecise)
+// - double (heavier)
+// - long double (the heaviest and the most precise)
+
+// Boolean:
+// - bool {true==1, false==0}
+
+// String:
+// Created as a string class for C++ STD library
+// Lots of different functions
+
+// Pointers:
+// Used to show the place of the objects and data on the PC
+
+// NB! -> use cppreference.com to get more information and types
+
