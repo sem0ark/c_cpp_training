50984662553776430b46deecc2039fd08ce670d9
sem0ark <arcadii.sem@gmail.com>
2022-12-09 18:51:35 +0200

    fix challenge.cpp, add card report

--- C++ introduction/001_learning_cpp/00-challenge-code/challenge.cpp
+++ C++ introduction/001_learning_cpp/00-challenge-code/challenge.cpp
@@ -84,35 +84,37 @@ float grade2f(char grd) {
   }
 }
 
-std::vector<Student> students = {
-  Student(1,"George P. Burdell"),
-  Student(2,"Nancy Rhodes")
-};
-
-std::vector<Course> courses = {
-  Course(1,"Algebra",5),
-  Course(2,"Physics",4),
-  Course(3,"English",3),
-  Course(4,"Economics",4)
-};
-
-std::vector<Grade> grades = {
-  Grade(1,1,'B'),
-  Grade(1,2,'A'),
-  Grade(1,3,'C'),
-  Grade(2,1,'A'),
-  Grade(2,2,'A'),
-  Grade(2,4,'B')
-};
 
 class Recorder{
 public:
-  void add_student(Student& student) { students.push_back(student); }
-  void add_student(Course& course)   { courses.push_back(course); }
-  void add_student(Grade& grade)     { grades.push_back(grade); }
+  void add_student(Student student) { students.push_back(student); }
+  void add_course(Course course)    { courses.push_back(course); }
+  void add_grade(Grade grade)       { grades.push_back(grade); }
 
   void student_report(int student_id) {
-    
+    int student_ind = 0;
+    while(students[student_ind].get_id() != student_id) student_ind++;
+
+    if(student_ind == students.size()) return;
+
+    std::cout << "Student name: " << students[student_ind].get_name() << std::endl;
+    std::cout << "Picked courses: " << std::endl;
+
+    for(auto& crs : courses) {
+      bool picked = false;
+      for(auto& grd : grades) {
+        if (grd.get_course_id() == crs.get_id() &&
+            grd.get_student_id() == students[student_ind].get_id()) {
+          picked = true;
+          break;
+        }
+      }
+      if(picked) {
+        std::cout << crs.get_name() << std::endl;
+      }
+    }
+    std::cout << "GPA: " << get_gpa(student_id) << std::endl;
+
   }
 
   float get_gpa(int student_id) {
@@ -139,8 +141,28 @@ private:
   std::vector<Grade> grades;
 };
 
+Recorder rec = Recorder();
 
 int main() {
-  std::cout << get_gpa(1) << std::endl;
+
+  
+  rec.add_student(Student(1,"George P. Burdell"));
+  rec.add_student(Student(2,"Nancy Rhodes"));
+
+  rec.add_course(Course(1,"Algebra",5));
+  rec.add_course(Course(2,"Physics",4));
+  rec.add_course(Course(3,"English",3));
+  rec.add_course(Course(4,"Economics",4));
+
+  
+  rec.add_grade(Grade(1,1,'B'));
+  rec.add_grade(Grade(1,2,'A'));
+  rec.add_grade(Grade(1,3,'C'));
+  rec.add_grade(Grade(2,1,'A'));
+  rec.add_grade(Grade(2,2,'A'));
+  rec.add_grade(Grade(2,4,'B'));
+
+  rec.student_report(1);
+
   return 0;
 }
